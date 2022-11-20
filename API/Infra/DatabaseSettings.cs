f16b48b651ec1189df27a547788c9f00b2b3e344
Luiz Fernando <lfernando488@gmail.com>
2022-11-19 21:43:57 -0300

    Mongo Repository

---
+++ API/Infra/DatabaseSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infra
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    public interface IDatabaseSettings
+    {
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+
+}
---
+++ API/Infra/MongoRepository.cs
@@ -0,0 +1,50 @@
+﻿using API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Infra
+{
+    public interface IMongoRepository<T>
+    {
+        List<T> Get();
+
+        T Get(string id);
+
+        T Create(T news);
+
+        void Update(string id, T newsIn);
+
+        void Remove(string id);
+    }
+
+    public class MongoRepository<T> : IMongoRepository<T> where T : BaseEntity
+    {
+
+        private readonly IMongoCollection<T> _model;
+
+        public MongoRepository(IDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _model = database.GetCollection<T>(typeof(T).Name.ToLower());
+        }
+
+        public T Create(T news)
+        {
+            _model.InsertOne(news);
+            return news;
+        }
+
+        public List<T> Get() => _model.Find(active => true).ToList();
+
+        public T Get(string id) =>
+            _model.Find<T>(news => news.Id == id).FirstOrDefault();
+
+        public void Remove(string id) => _model.DeleteOne(news => news.Id == id);
+
+        public void Update(string id, T newsIn) => _model.ReplaceOne(news => news.Id == id, newsIn);
+    }
+}
